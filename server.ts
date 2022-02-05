10dc5bcdae353d8646e962f80768560f8c46bab4
Yanan-G <yanangeng@Yanans-Mac-mini.local>
2022-02-05 14:29:33 -0500

    A1

--- server.ts
+++ server.ts
@@ -1,16 +1,10 @@
 import express from 'express';
 import bodyParser from "body-parser";
-import TuitController from './controllers/TuitController';
 
+import TuitController from './controllers/TuitController';
 import UserController from './controllers/UserController';
 import TuitDao from './daos/TuitDao';
 import UserDao from './daos/UserDao';
-
-// const dotenv = require('dotenv');
-// const connectDB = require('./config/db');
-
-// load env vars
-// dotenv.config({ path: './config/config.env' });
 
 const mongoose = require('mongoose');
 
