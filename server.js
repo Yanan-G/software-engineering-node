9f035e2ff1fd074e1cc4e16479dee57763d57368
Yanan-G <yanangeng@Yanans-Mac-mini.local>
2022-02-05 13:48:26 -0500

    A1

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 "use strict";
 exports.__esModule = true;
-var express = require("express");
-var bodyParser = require("body-parser");
+var express_1 = require("express");
+var body_parser_1 = require("body-parser");
 var TuitController_1 = require("./controllers/TuitController");
 var UserController_1 = require("./controllers/UserController");
 var TuitDao_1 = require("./daos/TuitDao");
@@ -12,9 +12,9 @@ var connectDB = require('./config/db');
 dotenv.config({ path: './config/config.env' });
 // Connect to database
 connectDB();
-var app = express();
+var app = (0, express_1["default"])();
 // Body parser
-app.use(bodyParser.json());
+app.use(body_parser_1["default"].json());
 var userController = new UserController_1["default"](app, new UserDao_1["default"]());
 var tuitController = new TuitController_1["default"](app, new TuitDao_1["default"]());
 //const PORT = 4000;
