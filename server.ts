73f24041fc77fe944c1593b8ccf89733187844eb
Yanan-G <yanangeng@Yanans-Mac-mini.local>
2022-02-05 14:18:28 -0500

    A1

--- server.ts
+++ server.ts
@@ -6,12 +6,27 @@ import UserController from './controllers/UserController';
 import TuitDao from './daos/TuitDao';
 import UserDao from './daos/UserDao';
 
-const dotenv = require('dotenv');
-const connectDB = require('./config/db');
+// const dotenv = require('dotenv');
+// const connectDB = require('./config/db');
 
 // load env vars
-dotenv.config({ path: './config/config.env' });
+// dotenv.config({ path: './config/config.env' });
 
+const mongoose = require('mongoose');
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
+
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+};
 // Connect to database
 connectDB();
 
